d1d98cd4926f08d1283bc2e3fcd67d5c29192953
Samuel W. Flint <swflint@flintfam.org>
2023-03-02 10:27:50 -0600

    Add in a stub .el file

---
+++ electric-ospl-mode.el
@@ -0,0 +1,18 @@
+;;; electric-ospl-mode.el --- Electric OSPL Mode -*- lexical-binding: t -*-
+
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;; SPDX-FileCopyrightText: 2023 Samuel W. Flint <swflint@flintfam.org>
+;; Homepage: https://git.sr.ht/~swflint/electric-ospl-mode
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.1"))
+
+;;; Commentary:
+;;
+;; Foo
+
+
+;;; Code:
+
+(provide 'electric-ospl-mode)
+
+;;; electric-ospl-mode.el ends here
