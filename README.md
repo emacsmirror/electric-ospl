b7faed4dfa11283519dd42b35ff8cbbd2cdb068a
Samuel W. Flint <swflint@flintfam.org>
2023-03-02 20:09:17 -0600

    Add documentation for electric-ospl

--- README.md
+++ README.md
@@ -2,13 +2,26 @@
 
 # electric-ospl-mode
 
+A simple minor mode to automatically enforce a "one-sentence per line" style in text files.
+
 ## Installation
 
-## Enabling
+Place the file `electric-ospl.el` somewhere on your `load-path`, and `require` it (note, autoloading is also supported).
+Enable `electric-ospl-mode` anywhere where you want to follow a "one-sentence per line" style, for instance, by adding to various mode hooks (I use `text-mode-hook` personally).
 
 ## Configuration
 
+There are a couple of options which can be used to modify behavior (and speed) of the mode.
+
+ - The first is `electric-ospl-regexps`, which sets the list of regular expressions defining how a sentence ends.
+ - The next is `electric-ospl-ignored-abbreviations`, which is a (case-sensitive) list of abbrevations ending in a period that are not necessarily considered the end of a sentence.
+ - Finally, efficiency may be modified by changing `electric-ospl-maximum-lookback-chars`, which determines how far to look back to find the end of a sentence.
+
+## Acknowledgements
+
+This code is based quite significantly on Jan Seeger's [`twauctex`](https://github.com/jeeger/twauctex).
+This package, however, is designed to simplify the logic and generalize it so that it is usable outside of `LaTeX-mode`.
+
 ## Errors and Patches
 
-If you find an error or have a patch to improve this package, please
-send an email to `~swflint/public-inbox@lists.sr.ht`.
+If you find an error or have a patch to improve this package, please send an email to `~swflint/public-inbox@lists.sr.ht`.
--- electric-ospl.el
+++ electric-ospl.el
@@ -1,8 +1,8 @@
 ;;; electric-ospl.el --- Electric OSPL Mode -*- lexical-binding: t -*-
 
 ;; Author: Samuel W. Flint <swflint@flintfam.org>
-;; Version: 0.0.1
-;; Package-Requires: ((emacs "24.1"))
+;; Version: 1.0.0
+;; Package-Requires: ((emacs "26.1") (s "1.11.0"))
 ;; Keywords: convenience, text
 ;; URL: https://git.sr.ht/~swflint/electric-ospl-mode
 ;; SPDX-License-Identifier: GPL-3.0-or-later
@@ -25,13 +25,45 @@
 
 ;;; Commentary:
 ;;
+;; A simple minor-mode to automatically enforce a "one-sentence per
+;; line" style in text files.
 ;;
+;;;; Installation
+;;
+;; Place the file `electric-ospl.el' somewhere on your `load-path',
+;; and `require` it (note, autoloading is also supported).  Enable
+;; `electric-ospl-mode' anywhere where you want to follow a
+;; "one-sentence per line" style, for instance, by adding to various
+;; mode hooks (I use `text-mode-hook' personally).
+;;
+;;;; Configuration
+;;
+;; There are a couple of options which can be used to modify behavior
+;; (and speed) of the mode.
+;;
+;; - The first is `electric-ospl-regexps', which sets the list of
+;;   regular expressions defining how a sentence ends.
+;;
+;; - The next is `electric-ospl-ignored-abbreviations', which is a
+;;   (case-sensitive) list of abbrevations ending in a period that are
+;;   not necessarily considered the end of a sentence.
+;;
+;; - Finally, efficiency may be modified by changing
+;;   `electric-ospl-maximum-lookback-chars', which determines how far
+;;   to look back to find the end of a sentence.
 ;;
 ;;;; Acknowledgments
+;;
 ;; This code is based significantly on Jan Seeger's `twauctex'
 ;; (https://github.com/jeeger/twauctex).  I've made effort to simplify
 ;; the logic and make it so that it's usable in modes other than
 ;; `LaTeX-mode'.
+;;
+;;;; Errors and Patches
+;;
+;; If you find an error or have a patch to improve this package, please
+;; send an email to `~swflint/public-inbox@lists.sr.ht`.
+
 
 
 ;;; Code:
